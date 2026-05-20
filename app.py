1755655eda94917837ecb9b8d1a1fa348c572ea8
alexandre-dremont <115954335+alexandre-dremont@users.noreply.github.com>
2026-05-19 23:01:05 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html
+
+
+app = dash.Dash(__name__)
+app.title = 'Project | INF8808'
+
+app.layout = html.Div([
+    html.H1("Voici notre projet de visualisation de données")
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
