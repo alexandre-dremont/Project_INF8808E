f62eb657c0595d92ebf99998a8c21b5ee2196cda
Benoît DUPIRE <benoitdupire06@gmail.com>
2026-06-06 20:22:07 -0400

    Update dumbbell_chart.py

--- components/dumbbell_chart.py
+++ components/dumbbell_chart.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import plotly.graph_objects as go
 from dash import Dash, dcc, html
 from data_preprocessing.dumbbell_data import load_data
