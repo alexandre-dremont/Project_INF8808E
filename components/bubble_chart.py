42ce70e468d99c4be961e7905ca3264156670b3f
alexandre-dremont <115954335+alexandre-dremont@users.noreply.github.com>
2026-06-12 22:40:35 -0400

    Update bubble_chart.py

--- components/bubble_chart.py
+++ components/bubble_chart.py
@@ -1,16 +1,17 @@
 import plotly.express as px
 from data_preprocessing.banque_mondiale import banque_mondiale_pre_processing
-from data_preprocessing.obesity_prevalence import obesity_prevalence_pre_processing
+from data_preprocessing.obesity_prevalence import obesity_prevalence_pre_processing, obesity_prevalence_pre_processing_without_year
 import pandas as pd
 
 def create_bubble_chart():
     df_indicator = banque_mondiale_pre_processing(scale=False)
-    df_obesity = obesity_prevalence_pre_processing()
+    df_obesity = obesity_prevalence_pre_processing_without_year()
 
     # print(df_indicator.index)
     # print(df_obesity["Country"])
 
     df = df_indicator.merge(df_obesity, left_on="Country Name", right_on="Country")
+    df = df.dropna(subset=["Health_Expenditure"])
 
     # print(df)
 
@@ -23,10 +24,67 @@ def create_bubble_chart():
         # animation_frame='Year',
         # animation_group='Country Name',
         # log_x=True,
-        log_y=True,
-        hover_data="Country",
+        # log_y=True,
+        custom_data=["Country", "Income_group", "Year"],
         color_discrete_sequence=px.colors.qualitative.Set1,
-        size_max=100,
+        size_max=70,
+        labels={
+            "GDP_PPP" : "PIB par habitant en parité de pouvoir d'achat",
+            "All_Adults_Obesity": "Prévalence de l'obésité (%)",
+            "Health_Expenditure": "Dépenses de santé par habitant",
+            "Income_group": "Groupe de revenus"
+        }
+    )
+
+    fig.update_traces(hovertemplate=hover_tempate())
+
+    fig.update_layout(
+        # Style de plot
+        template="plotly_white",
+
+        # Info-bulles
+        hovermode="closest",
+        hoverlabel=dict(bgcolor="#ffffff", bordercolor="#e2e8f0",
+                        font=dict(family="Inter, sans serif", size=12, color="#2c3e50")),
+
+        # Légende
+        legend=dict(
+            title=dict(text="Groupe de revenus"),
+            font=dict(family="Inter, sans serif", size=12, color="#718096"),
+            orientation="h", yanchor="top",
+            y=-0.3, xanchor="center", x=0.5,
+            bordercolor="#e2e8f0", borderwidth=1, 
+            bgcolor="#ffffff"
+        ),
+
+        # Axes
+        xaxis=dict(title=dict(text="Prévalence de l'obésité (%)", 
+                   font=dict(family="Inter, sans serif", size=12, color="#718096")), 
+                   tickfont=dict(family="Inter, sans serif", size=11, color="#718096"),
+                   gridcolor="rgba(0,0,0,0.05)"),
+
+        yaxis=dict(title="PIB par habitant en parité de pouvoir d'achat (en US$)", tickfont=dict(family="Inter, sans serif", size=12, color="#2c3e50"),
+                   ticklabelstandoff=10),
+    )
+
+    fig.update_xaxes(
+        title_font=dict(family="Inter, sans serif", size=12, color="#718096"),
+        tickfont=dict(family="Inter, sans serif", size=11, color="#718096")
+    )
+
+    fig.update_yaxes(
+        title_font=dict(family="Inter, sans serif", size=12, color="#718096"),
+        tickfont=dict(family="Inter, sans serif", size=11, color="#718096"),
     )
 
     return fig
+
+def hover_tempate():
+    return (
+        '<b>%{customdata[0]}</b>'
+        "<br>Prévalence de l'obésité : %{x:.1f}%"
+        "<br>PIB par habitant (PPA) : %{y:,.0f} $"
+        '<br>Année : %{customdata[2]}'
+        '<br>Groupe de revenus : %{customdata[1]}'
+        '<extra></extra>'
+    )
